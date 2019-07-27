65103de7a451d73a2fc61d226b3484d72c2e7feb
Álex González <agonzalezro@gmail.com>
2019-07-27 17:05:48 +0200

    Updates

---
+++ precache-manifest.a7c221929a34a718efcd3e6768565577.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77a16d2698a5fe3232f1b84caddf360d",
+    "url": "/wikiur/index.html"
+  },
+  {
+    "revision": "49fec63f4d14544698bc",
+    "url": "/wikiur/static/css/main.3851ed92.chunk.css"
+  },
+  {
+    "revision": "620f10e537cc3e4e1889",
+    "url": "/wikiur/static/js/2.5a685ebc.chunk.js"
+  },
+  {
+    "revision": "49fec63f4d14544698bc",
+    "url": "/wikiur/static/js/main.69bc91eb.chunk.js"
+  },
+  {
+    "revision": "18e2869f8b3b958ade18",
+    "url": "/wikiur/static/js/runtime~main.e1bdad46.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.892334bbbc5ac7bbbe4cf65eceba507c.js"
+  "/wikiur/precache-manifest.a7c221929a34a718efcd3e6768565577.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wikiur/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
