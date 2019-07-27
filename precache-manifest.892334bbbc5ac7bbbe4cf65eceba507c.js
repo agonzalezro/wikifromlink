5badef350de383c110c44612c9bb33399ef47111
Álex González <agonzalezro@gmail.com>
2019-07-27 17:02:07 +0200

    Updates

---
+++ precache-manifest.892334bbbc5ac7bbbe4cf65eceba507c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "675c6a0f176d1a5455f535af36a2261d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49fec63f4d14544698bc",
+    "url": "/static/css/main.3851ed92.chunk.css"
+  },
+  {
+    "revision": "620f10e537cc3e4e1889",
+    "url": "/static/js/2.5a685ebc.chunk.js"
+  },
+  {
+    "revision": "49fec63f4d14544698bc",
+    "url": "/static/js/main.69bc91eb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
