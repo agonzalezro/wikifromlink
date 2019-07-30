6dc572911000a18765f2146922f411c9f9d94ba2
Álex González <agonzalezro@gmail.com>
2019-07-30 18:51:42 +0200

    Typo: lastest > latest

--- src/WarningAboutURLSharing.js
+++ src/WarningAboutURLSharing.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class WarningAboutURLSharing extends Component {
     render() {
         return <div className="alert alert-warning" role="alert">
-            <i className="fas fa-anchor"></i> Remember to copy the lastest URL (it will change anytime you add something) to share the latest changes.
+            <i className="fas fa-anchor"></i> Remember to copy the latest URL (it will change anytime you add something) to share the latest changes.
         </div>
     }
 }
